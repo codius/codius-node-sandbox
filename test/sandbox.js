97dd63466ba2cac08139527e937fe440845f16e4
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-06-19 11:51:56 -0700

    switching from should to chai/expect/sinon

--- test/sandbox.js
+++ test/sandbox.js
@@ -2,76 +2,106 @@
 // Init
 //-----------------------------------------------------------------------------
 
-var should  = require('should');
-var sinon   = require('sinon');
-var Sandbox = require('../lib/sandbox');
+var chai      = require('chai');
+var expect    = chai.expect;
+var sinon     = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+var Sandbox   = require('../lib/sandbox');
 
 //-----------------------------------------------------------------------------
 // Tests
 //-----------------------------------------------------------------------------
 
 describe('Sandbox', function() {
+
   var sb;
   beforeEach(function(){
+
+    // this.timeout =   10000;
+
     sb = new Sandbox();
+    sb.options.timeout = 1000;
   });
 
+  // afterEach(function(){
+  //   sb = null;
+  // });
+
   it('should execute basic javascript', function(done) {
-    sb.run('1 + 1', function(output) {
-      output.result.should.eql('2');
+    sb.run('1 + 1', function(err, result) {
+      expect(err).not.to.exist;
+      expect(result).to.equal('2');
       done();
     });
   });
 
   it('should gracefully handle syntax errors', function(done) {
-    sb.run('hi )there', function(output) {
-      output.result.should.eql("'SyntaxError: Unexpected token )'");
+    sb.run('hi )there', function(err, result) {
+      expect(err).to.equal('SyntaxError: Unexpected token )');
+      expect(result).not.to.exist;
       done();
     });
   });
 
   it('should effectively prevent code from accessing node', function(done) {
-    sb.run('process.platform', function(output) {
-      output.result.should.eql("null");
+    sb.run('process.platform', function(err, result) {
+      expect(err).not.to.exist;
+      expect(result).to.equal('null');   
       done();
     });
   });
 
-  it('should effectively prevent code from circumventing the sandbox', function(done) {
-    sb.run("var sys=require('sys'); sys.puts('Up in your fridge')", function(output) {
-      output.result.should.eql("'ReferenceError: require is not defined'");
+  it.only('should effectively prevent code from circumventing the sandbox', function(done) {
+    sb.run("var sys=require('sys'); sys.puts('Up in your fridge')", function(err, result) {
+      expect(err).to.equal('ReferenceError: require is not defined');
+      expect(result).not.to.exist;
       done();
     });
   });
 
   it('should timeout on infinite loops', function(done) {
-    sb.run('while ( true ) {}', function(output) {
-      output.result.should.eql('TimeoutError');
+    sb.run('while ( true ) {}', function(err, result) {
+      expect(err).to.equal('TimeoutError');
+      expect(result).not.to.exist;
       done();
     });
   });
 
   it('should allow console output via `console.log`', function(done) {
-    sb.run('console.log(7); 42', function(output) {
-      output.result.should.eql('42');
-      output.console[0].should.eql(7);
+    var stdout_write = sinon.spy();
+    sb._stdout = { write: stdout_write };
+    
+    sb.run('console.log(7); 42', function(err, result) {
+      expect(err).not.to.exist;
+      expect(result).to.equal('42');
+      expect(stdout_write).to.be.calledOnce;
+      expect(stdout_write).to.be.calledWith('7');
       done();
     });
   });
 
   it('should allow console output via `print`', function(done) {
-    sb.run('print(7); 42', function(output) {
-      output.result.should.eql('42');
-      output.console[0].should.eql(7);
+    var stdout_write = sinon.spy();
+    sb._stdout = { write: stdout_write };
+
+    sb.run('print(7); 42', function(err, result) {
+      expect(err).not.to.exist;
+      expect(result).to.equal('42');
+      expect(stdout_write).to.be.calledWithExactly(7);
       done();
     });
   });
 
   it('should maintain the order of sync. console output', function(done) {
-    sb.run('console.log("first"); console.log("second"); 42', function(output) {
-      output.result.should.eql('42');
-      output.console[0].should.eql('first');
-      output.console[1].should.eql('second');
+    var stdout_write = sinon.spy();
+    sb._stdout = { write: stdout_write };
+
+    sb.run('console.log("first"); console.log("second"); 42', function(err, result) {
+      expect(err).not.to.exist;
+      expect(result).to.equal('42');
+      expect(stdout_write).on.firstCall.to.be.calledWith('first');
+      expect(stdout_write).on.secondCall.to.be.calledWith('second');
       done();
     });
   });
@@ -80,8 +110,8 @@ describe('Sandbox', function() {
     var messageHandler = sinon.spy();
     sb.on('message', messageHandler);
     sb.run('postMessage("Hello World!");', function(output){
-      messageHandler.calledOnce.should.eql(true);
-      messageHandler.calledWith('Hello World!').should.eql(true);
+      expect(messageHandler).to.be.calledOnce;
+      expect(messageHandler).to.be.calledWith('Hello World!');
       done();
     });
   });
@@ -92,9 +122,9 @@ describe('Sandbox', function() {
     sb.on('ready', function () {
       sb.postMessage('Hello World!');
     });
-    sb.run('onmessage = function (msg) { postMessage(msg); };', function(output) {
-      messageHandler.callCount.should.eql(1);
-      messageHandler.calledWith('Hello World!').should.eql(true);
+    sb.run('onmessage = function (msg) { postMessage(msg); };', function(err, result) {
+      expect(messageHandler).to.be.calledOnce;
+      expect(messageHandler).to.be.calledWith('Hello World!');
       done();
     });
   });
@@ -106,9 +136,9 @@ describe('Sandbox', function() {
       sb.postMessage(++num_messages_sent);
     }, 1);
     sb.on('message', messageHandler);
-    sb.run('onmessage = function (msg) { postMessage(msg); };', function(output) {
-      messageHandler.callCount.should.eql(num_messages_sent);
-      num_messages_sent.should.be.greaterThan(0);
+    sb.run('onmessage = function (msg) { postMessage(msg); };', function(err, result) {
+      expect(messageHandler).to.have.callCount(num_messages_sent);
+      expect(num_messages_sent).to.be.greaterThan(0);
       done();
     });
     sb.on('ready', function(){
