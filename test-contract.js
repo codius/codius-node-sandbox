68c960030e7bbcb68ffc7a380073f3a5053c4684
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-10-02 13:48:06 -0700

    [TASK] Update log messages for contract

--- test-contract.js
+++ test-contract.js
@@ -27,11 +27,14 @@ function createServer(PORT, CONTRACT_PRIVKEY) {
 			var transaction = bitcoin.Transaction.fromHex(json.transaction);
 			var redeem_script = bitcoin.Script.fromHex(json.redeem_script);
 
-			console.log('Contract got request to sign transaction with hash: ', transaction.getId());
+			console.log('Contract got request to sign transaction with hash: ', transaction.getId() + '\n');
 
 			var key = bitcoin.ECKey.fromWIF(CONTRACT_PRIVKEY);
 			
 			var transaction_builder = bitcoin.TransactionBuilder.fromTransaction(transaction);
+
+			console.log('...Just a few more seconds... We\'re still experiencing performance issues...\n');
+
 			transaction_builder.sign(0, key, redeem_script);
 
 			var signature = transaction_builder.signatures[0].signatures[0];
@@ -40,7 +43,8 @@ function createServer(PORT, CONTRACT_PRIVKEY) {
 			res.write(signature_hex);
 			res.end();
 
-			console.log('Contract computed signature: ' + signature_hex);
+			console.log('Contract computed signature: ' + signature_hex + '\n');
+			console.log('Sending signature back to client...');
 
 		}));
 	}
@@ -48,4 +52,9 @@ function createServer(PORT, CONTRACT_PRIVKEY) {
 	http.createServer(responseHandler).listen(PORT, HOST);
 
 	console.log('Contract listening on port: ' + PORT);
+
+	console.log('Welcome to the ...');
+
+	console.log('\n  ______                   __  __                            _______  ________   ______         __       __            __  __             __     \n /      \\                 |  \\|  \\                          |       \\|        \\ /      \\       |  \\  _  |  \\          |  \\|  \\           |  \\    \n|  $$$$$$\\  ______    ____| $$ \\$$ __    __   _______       | $$$$$$$\\\\$$$$$$$$|  $$$$$$\\      | $$ / \\ | $$  ______  | $$| $$  ______  _| $$_   \n| $$   \\$$ /      \\  /      $$|  \\|  \\  |  \\ /       \\      | $$__/ $$  | $$   | $$   \\$$      | $$/  $\\| $$ |      \\ | $$| $$ /      \\|   $$ \\  \n| $$      |  $$$$$$\\|  $$$$$$$| $$| $$  | $$|  $$$$$$$      | $$    $$  | $$   | $$            | $$  $$$\\ $$  \\$$$$$$\\| $$| $$|  $$$$$$\\\\$$$$$$  \n| $$   __ | $$  | $$| $$  | $$| $$| $$  | $$ \\$$    \\       | $$$$$$$\\  | $$   | $$   __       | $$ $$\\$$\\$$ /      $$| $$| $$| $$    $$ | $$ __ \n| $$__/  \\| $$__/ $$| $$__| $$| $$| $$__/ $$ _\\$$$$$$\\      | $$__/ $$  | $$   | $$__/  \\      | $$$$  \\$$$$|  $$$$$$$| $$| $$| $$$$$$$$ | $$|  \\\n \\$$    $$ \\$$    $$ \\$$    $$| $$ \\$$    $$|       $$      | $$    $$  | $$    \\$$    $$      | $$$    \\$$$ \\$$    $$| $$| $$ \\$$     \\  \\$$  $$\n  \\$$$$$$   \\$$$$$$   \\$$$$$$$ \\$$  \\$$$$$$  \\$$$$$$$        \\$$$$$$$    \\$$     \\$$$$$$        \\$$      \\$$  \\$$$$$$$ \\$$ \\$$  \\$$$$$$$   \\$$$$')
+	console.log('\nPlease submit a transaction for signing :)\n');
 }
